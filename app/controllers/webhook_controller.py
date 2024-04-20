58020d58e961f7468f88f6818e89a6dc396d118f
Leonardo Meireles <meirelesleonardo.ti@gmail.com>
2024-04-20 00:44:32 -0300

    feat: Implementa deploy_api_gestao

--- app/controllers/webhook_controller.py
+++ app/controllers/webhook_controller.py
@@ -15,7 +15,7 @@ webhook_bp = Blueprint('webhook', __name__, url_prefix='/webhook')
 WEBHOOK_SECRET = variabbles.WEBHOOK_SECRET_DEPLOY
 
 @webhook_bp.route('/deploy-connect-city', methods=['POST'])
-def deploy():
+def deploy_connect_city():
     signature = request.headers.get('X-Hub-Signature')
     if not signature:
         abort(400, "Signature header is missing")
@@ -30,6 +30,26 @@ def deploy():
         abort(403, "Invalid signature")
 
     # Chama o script de deploy
-    #subprocess.run([variabbles.DEPLOY_CONNECT_CITY])
+    subprocess.run([variabbles.DEPLOY_CONNECT_CITY])
+
+    return "Deployment started", 200
+
+@webhook_bp.route('/deploy-api-gestao', methods=['POST'])
+def deploy_api_gestao():
+    signature = request.headers.get('X-Hub-Signature')
+    if not signature:
+        abort(400, "Signature header is missing")
+
+    sha_name, signature = signature.split('=')
+    if sha_name != 'sha1':
+        abort(501, "Unsupported signature type")
+
+    mac = hmac.new(bytes(WEBHOOK_SECRET, 'utf-8'), msg=request.data, digestmod=hashlib.sha1)
+    assinatura = str(mac.hexdigest())
+    if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
+        abort(403, "Invalid signature")
+
+    # Chama o script de deploy
+    subprocess.run([variabbles.DEPLOY_API_GESTAO])
 
     return "Deployment started", 200
